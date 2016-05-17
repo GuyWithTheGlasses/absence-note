2a16a563d3a6db3ee5f87b56fee94db321a0bd22
Leon Chou <bleepbloopsify@gmail.com>
2016-05-17 14:38:17 -0400

    pls

--- ownerstartup.sh
+++ ownerstartup.sh
@@ -4,7 +4,5 @@ export env=developement
 export OWNER="Is owner"
 
 export SECRET=123124yas9aidj/asdaof11921oas
-export FACEBOOK_APP_ID=213178392392501
-export FACEBOOK_APP_SECRET=bcac09923ce22feb8d262158721b864d
 
 node ./bin/www
