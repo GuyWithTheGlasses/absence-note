cc1a82ef60ea968b5d78ab46de5bbaf7e33e2a72
Leon Chou <bleepbloopsify@gmail.com>
2016-05-22 19:44:57 -0400

    pls

--- handlers/students/index.js
+++ handlers/students/index.js
@@ -1,35 +1,45 @@
 var templates = require('../../config/templates');
+var Absence = require('../../models/absences').Absence;
 module.exports = {
   'check': {
-    'loggedIn':function(req,res,next){
+    'loggedIn': function(req, res, next) {
       console.log('hi');
-      if(req.isAuthenticated()) return next();
+      if (req.isAuthenticated()) return next();
       else return res.redirect('/login');
     }
   },
   'index': {
     'get': function(req, res, next) {
-      res.render(templates.students.index, {user:req.user});
+      res.render(templates.students.index, { user: req.user });
     }
   },
-  'absencenote':{
-    get:function(req,res, next){
-      if(!req.user) return next('Missing User');
-      res.render(templates.students.createabsencenote, {user:req.user});
+  'absencenote': {
+    get: function(req, res, next) {
+      if (!req.user) return next('Missing User');
+      res.render(templates.students.createabsencenote, { user: req.user });
     },
-    post: function(req, res){
+    post: function(req, res) {
       var student = req.user.google.name;
-      var note = {
-        student: student,
-        OSIS:req.body.OSIS,
-        homeroom:req.user.homeroom,
-        excused:req.body.excused || null,
-        corrections:req.body.corrections || null,
-        submission_date: req.body.submission_date,
-        excused_date: req.body.excused_date,
-        excuse:String
-      };
-      res.render(templates.students.viewabsencenote, {user:req.user, note:note});
+      var note = new Absence();
+      note.student = student;
+      note.OSIS = req.body.OSIS;
+      note.homeroom = req.user.homeroom;
+      note.excused = req.body.excused || null;
+      note.corrections = req.body.corrections || null;
+      note.submission_date = req.body.submission_date;
+      note.excused_date = req.body.excused_date;
+      note.excuse = req.body.excuse;
+      note.save(function(err){
+        if(err) return res.send(err);
+        return res.send({
+          'success':true,
+          'note':note
+        });
+      });
+    },
+    'id':{
+      get:function(req,res,next){},
+      post:function(req,res,next){}
     }
   }
 };
--- routes/students/index.js
+++ routes/students/index.js
@@ -6,7 +6,12 @@ var handlers = require('../../handlers/students');
 router.use(handlers.check.loggedIn);
 
 router.get('/', handlers.index.get);
+router.get('/absencenote/:id', handlers.absencenote.id.get);
+
+
+
 router.get('/absencenote', handlers.absencenote.get);
 router.post('/absencenote', handlers.absencenote.post);
+
 
 module.exports = router;
