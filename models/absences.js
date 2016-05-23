13e4bb7e40fff10f8abeb6da45afd5d3b3f997cc
Johnny So <jso123450@gmail.com>
2016-05-22 21:36:50 -0400

    added sched

--- models/absences.js
+++ models/absences.js
@@ -13,6 +13,18 @@ var absenceSchema = mongoose.Schema({
     'Signature': { type: String, default: 'Unsigned' },
     'Name': String,
     "Date of Signature": String
+  }),
+  schedule : new mongoose.Schema({
+      'Period 1': String,
+      'Period 2': String,
+      'Period 3': String,
+      'Period 4': String,
+      'Period 5': String,
+      'Period 6': String,
+      'Period 7': String,
+      'Period 8': String,
+      'Period 9': String,
+      'Period 10': String
   })
 });
 
