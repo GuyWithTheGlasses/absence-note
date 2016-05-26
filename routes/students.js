8fb3ae90414abf4557877e6984fe2168edb372ab
Leon Chou <bleepbloopsify@gmail.com>
2016-05-26 17:30:58 -0400

    pls

--- routes/students.js
+++ routes/students.js
@@ -6,7 +6,6 @@ var handlers = require('../handlers/students');
 router.use(handlers.check.loggedIn);
 
 router.get('/', handlers.index.get);
-router.get('/absence/:id', handlers.absences.id.get);
 
 router.get('/earlyexcuse/:id', handlers.earlyexcuse.id.get);
 router.get('/earlyexcuse', handlers.earlyexcuse.get);
@@ -14,6 +13,7 @@ router.get('/earlyexcuse', handlers.earlyexcuse.get);
 router.get('/absence/create', handlers.absences.create.get);
 router.post('/absence/create', handlers.absences.create.post);
 
+router.get('/absence/:id', handlers.absences.id.get);
 router.get('/profile', handlers.profile.get);
 
 module.exports = router;
