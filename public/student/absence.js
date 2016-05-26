849845fa09a556958377cebd142cb773519fa3a5
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-05-25 23:12:20 -0400

    ajax

--- public/student/absence.js
+++ public/student/absence.js
@@ -1,12 +1,15 @@
 var ABSENCE_FORM_CLASS = 'absence-form';
 var SUBMIT_BUTTON = 'form-submit-ajax';
+var PDF_BUTTON = 'form-create-pdf';
 var ERROR_MESSAGE = 'error-message';
 
 var setupForms = function(event){
   forEachInClass(document, ABSENCE_FORM_CLASS, function(form){
     form.onsubmit = form.onsubmit || function(event){event.preventDefault();};
-      
-    var submitForm = function(event){
+      var getData = function(event){
+	  
+      };
+      var submitForm = function(event){
       submit(form, {
         url:'/students/absencenote',
         method:'POST',
@@ -25,6 +28,9 @@ var setupForms = function(event){
     };
     forEachInClass(form, SUBMIT_BUTTON, function(button){
 	button.addEventListener('click', submitForm);
+    });
+    forEachInClass(form, PDF_BUTTON, function(button){
+	button.addEventListener('click', getData);
     });
   });
 };
