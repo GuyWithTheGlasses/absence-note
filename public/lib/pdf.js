7e975cafb6195128e97648e5f3190ff39e57751e
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-05-27 16:32:08 +0000

    submitting forms to server and generating pdf

--- public/lib/pdf.js
+++ public/lib/pdf.js
@@ -1,27 +1,37 @@
 var PDFDocument = require("pdfkit");
 
 //Takes data object from user input
-//Creates absence/excuse note PDF using inputed data 
-var createPDF = function(data){
+//Creates absence note PDF using inputed data 
+var createAbsencePDF = function(data) {
 
-    //Create new PDF document 
-    var doc = new PDFDocument();
-    
-    //Pipe to a readable Node stream - what does this mean?
-    var stream = doc.pipe(res);
-    
-    //Create heading (same for every note)
-    doc.fontSize(20)
-	.text("STUYVESANT HIGH SCHOOL", { align: "center" })
-	.text("ATTENDANCE OFFICE", { align: "center" })
-	.text("ROOM 203", { align: "center" });   
-    
-    //MORE DRAWING TO TAKE PLACE HERE
+  //Create new PDF document 
+  var doc = new PDFDocument();
 
-    //End the doc 
-    doc.end();
-    //When the stream is finished, do something (ask how to display)
-    stream.on("finish", function(){
-	console.log(doc);
+  //Pipe to a readable Node stream - what does this mean?
+  var stream = doc.pipe(res);
+
+  //Create heading (same for every note)
+  doc.fontSize(20)
+    .text("STUYVESANT HIGH SCHOOL", {
+      align: "center"
+    })
+    .text("ATTENDANCE OFFICE", {
+      align: "center"
+    })
+    .text("ROOM 203", {
+      align: "center"
     });
+
+  doc.text(data[])
+
+  //End the doc 
+  doc.end();
+  //When the stream is finished, do something (ask how to display)
+  stream.on("finish", function() {
+    console.log(doc);
+  });
+};
+
+var createExcusePDF = function(data) {
+
 };
