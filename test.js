b68f68ffee9094211bc45d84dc99d9f97afaa8de
Johnny So <jso123450@gmail.com>
2016-06-07 23:50:36 -0400

    fixed test.js

--- test.js
+++ test.js
@@ -1,41 +1,44 @@
 // var Absence = require('./models/absences').Absence;
 // var Teacher = require('./models/accounts').Teacher;
 var notes = require('./models/notes');
+var Absence = notes.Absence;
+var EarlyExcuse = notes.EarlyExcuse;
+var Note = notes.Note;
 var accounts = require('./models/accounts');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/absence');
 
 var teacher = new accounts.Teacher();
 
-var note = new notes.Absence();
-note.student = 'Leon Chou';
-note.OSIS = 203766068;
-note.homeroom = '7QQ';
-note.excused = 'absence-excuse';
-note.corrections = null;
-note.submission_date = "05/21";
-note.excused_date = "05/14";
-note.excuse = "Went to dentist";
+var new_note = new Absence();
+new_note.student = 'Leon Chou';
+new_note.OSIS = 203766068;
+new_note.homeroom = '7QQ';
+new_note.excused = 'absence-excuse';
+new_note.corrections = null;
+new_note.submission_date = "05/21";
+new_note.excused_date = "05/14";
+new_note.excuse = "Went to dentist";
 
 /* ------------------ ADDS ---------------- */
-// note.add( function(err){
+// new_note.add( function(err){
 //     if (err)
 // 	return console.log( err );
 //     return;
 // });
 
 /* ------------------ REMOVES --------------------- */
-// notes.Absence.remove({excused_date: note.excused_date} ,function(err,removed){
+// Absence.remove({excused_date: new_note.excused_date} ,function(err,removed){
 //     console.log('err');
 //   //console.log(removed);
 // });
 
 
 /* ------------------ QUERIES -------------------- */
-// notes.Absence.find(function(err,absences){
+// Absence.find(function(err,absences){
 //   console.log('err', err);
 //   console.log('absences', absences);
-//   notes.Absence.findById(absences[0]._id, function(err, absence){
+//   Absence.findById(absences[0]._id, function(err, absence){
 //     absence.approve(function(err){
 //       if(err) console.log(err);
 //     });
@@ -45,19 +48,19 @@ note.excuse = "Went to dentist";
 //   });
 // });
 
-notes.Note.find(function(err,notes){
-  console.log('err',err);
-  console.log('notes',notes);
-  console.log('notes[0]',notes[0]);
-  console.log('notes[0].student',notes[0].student);
-  notes.Note.findById(notes[0]._id,function(err,note){
-    console.log(note);
-  });
-});
+// Note.find(function(err,notes){
+//   console.log('err',err);
+//   console.log('notes',notes);
+//   console.log('notes[0]',notes[0]);
+//   console.log('notes[0].student',notes[0].student);
+//   Note.findById(notes[0]._id,function(err,note){
+//     console.log(note);
+//   });
+// });
 
 
 /* ----------------- DELETES FROM ALL TEACHERS -------------- */
-// note.delete(function(err){
+// new_note.delete(function(err){
 //    if (err)
 //     return console.log(err);
 //   return;
