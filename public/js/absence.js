441a703997faff72f220c8c6a52665015418ca2f
captiosus <royxu652@yahoo.com>
2016-06-08 12:05:23 -0400

    absence note updates

---
+++ public/js/absence.js
@@ -0,0 +1,55 @@
+var correctionBox = document.getElementById( "correction-box" );
+var excusedBox = document.getElementById( "excused-box" );
+var masterBox = document.getElementById( "master-box" );
+
+var correction = document.getElementById( "correction" );
+var excused = document.getElementById( "excused" );
+
+correction.addEventListener( "click", function( e ) {
+  e.preventDefault();
+  correctionBox.style.display = "block";
+  excusedBox.style.display = "none";
+} );
+
+excused.addEventListener( "click", function( e ) {
+  e.preventDefault();
+  excusedBox.style.display = "block";
+  correctionBox.style.display = "none";
+} );
+
+var ABSENCE_FORM_CLASS = 'absence-form';
+var SUBMIT_BUTTON = 'form-submit-ajax';
+var PDF_BUTTON = 'form-create-pdf';
+var TEXT_INPUT = 'input';
+var EXPLANATION_INPUT = 'explanation';
+var PERIOD_CHECKBOX = 'checkbox';
+var FORM_ENTRIES = 'form-entry';
+var ERROR_MESSAGE = 'error-message';
+var RADIO_BUTTON_DIV = 'master-options';
+var RADIO_BUTTON = 'excusemestopreading';
+
+var setupForms = function( event ) {
+  //Add click listeners to all buttons in form
+  forEachInClass( document, RADIO_BUTTON, function( radio_button ) {
+    radio_button.addEventListener( 'click', function( e ) {
+      e.preventDefault();
+      //When this button is clicked
+      //Delete all other "clicked" classes
+      if ( this.id != "excused" && this.id != "correction" ) {
+        forEachInClass( document, RADIO_BUTTON, function( radio_button ) {
+          if ( radio_button.id != "excused" && radio_button.id != "correction" ) {
+            radio_button.classList.remove( 'clicked' );
+          }
+        } );
+      } else {
+        forEachInClass( document, RADIO_BUTTON, function( radio_button ) {
+          radio_button.classList.remove( 'clicked' );
+        } );
+        //Add the "clicked" class to this button
+      }
+      this.classList.add( 'clicked' );
+    } );
+  } );
+};
+
+document.addEventListener( 'DOMContentLoaded', setupForms );
---
+++ public/js/profile.js
@@ -0,0 +1,51 @@
+var teachers = [ "Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails" ];
+
+var oneName = document.getElementById( "1name" );
+new Awesomplete( oneName, {
+  list: teachers
+} );
+
+var twoName = document.getElementById( "2name" );
+new Awesomplete( twoName, {
+  list: teachers
+} );
+
+var threeName = document.getElementById( "3name" );
+new Awesomplete( threeName, {
+  list: teachers
+} );
+
+var fourName = document.getElementById( "4name" );
+new Awesomplete( fourName, {
+  list: teachers
+} );
+
+var fiveName = document.getElementById( "5name" );
+new Awesomplete( fiveName, {
+  list: teachers
+} );
+
+var sixName = document.getElementById( "6name" );
+new Awesomplete( sixName, {
+  list: teachers
+} );
+
+var sevenName = document.getElementById( "7name" );
+new Awesomplete( sevenName, {
+  list: teachers
+} );
+
+var eightName = document.getElementById( "8name" );
+new Awesomplete( eightName, {
+  list: teachers
+} );
+
+var nineName = document.getElementById( "9name" );
+new Awesomplete( nineName, {
+  list: teachers
+} );
+
+var tenName = document.getElementById( "10name" );
+new Awesomplete( tenName, {
+  list: teachers
+} );
