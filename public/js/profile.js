19d0a583f9b037b230de1c6ce90664eac9dd3a6f
captiosus <royxu652@yahoo.com>
2016-06-10 02:09:52 -0400

    fix and profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,15 @@
-var teachers = [ "Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails" ];
+ajax( {
+  url: '/student/email',
+  method: 'GET',
+  success: function( res ) {
+    for ( var x = 1; x < 11; x++ ) {
+      var name = completely( document.getElementById( x + 'name' ) );
+      name.options = res.split( ";" );
+      name.startFrom = 0;
+    }
+  }
 
-var auto = completely( document.getElementById( '1name' ) );
-
-auto.options = teachers;
-auto.startFrom = 0;
+} );
 
 var insert = '<input type="text" class="input save-text"><i class="fa fa-floppy-o" aria-hidden="true"></i>';
 var done = '<i class="fa fa-pencil" aria-hidden="true"></i>';
@@ -22,4 +28,20 @@ for ( var x = 0; x < pencils.length; x++ ) {
       parent.innerHTML = done;
     } );
   } );
+}
+
+var variable = document.getElementsByClassName( "variable" );
+
+for ( var x = 0; x < variable.length; x++ ) {
+  if ( ( variable[ x ].innerHTML ).trim() === "" ) {
+    console.log( "hi" );
+    variable[ x ].innerHTML = insert;
+    variable[ x ].childNodes[ 0 ].focus();
+    variable[ x ].childNodes[ 1 ].addEventListener( "click", function( e ) {
+      e.preventDefault();
+      //add ajax here
+      var parent = this.parentNode;
+      parent.innerHTML = done;
+    } );
+  }
 }
