42ceaaaa6f3f0d47be6963f0cff631c328c49b14
captiosus <royxu652@yahoo.com>
2016-06-10 09:17:27 -0400

    approve and deny

--- public/js/history.js
+++ public/js/history.js
@@ -1,8 +1,14 @@
-var rows = document.getElementsByTagName( "tr" );
+var rows = document.getElementsByClassName( "tablerow" );
 
 for ( var x = 0; x < rows.length; x++ ) {
-  rows[ x ].addEventListener( "click", function( e ) {
-    e.preventDefault();
-    window.location = ( this.children[ 0 ].children[ 0 ].getAttribute( "href" ) );
-  } );
+  var link = rows[ x ].children[ 0 ].children[ 0 ].getAttribute( "href" );
+  var cells = rows[ x ].children;
+  for ( var y = 0; y < cells.length; y++ ) {
+    if ( cells[ y ].classList.contains( "tablelink" ) ) {
+      cells[ y ].addEventListener( "click", function( e ) {
+        e.preventDefault();
+        window.location = link;
+      } );
+    }
+  }
 }
