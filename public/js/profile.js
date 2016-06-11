2fce57aa0c2991fae9ab0c751eaa558b48765bc6
captiosus <royxu652@yahoo.com>
2016-06-10 22:28:53 -0400

    min

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,95 +1,95 @@
-ajax({
+ajax( {
   url: '/student/email',
   method: 'GET',
-  success: function(res) {
-    for (var x = 1; x < 11; x++) {
-      var name = completely(document.getElementById(x + 'name'));
-      name.options = res.split(";").sort();
+  success: function( res ) {
+    for ( var x = 1; x < 11; x++ ) {
+      var name = completely( document.getElementById( x + 'name' ) );
+      name.options = res.split( ";" ).sort();
       name.startFrom = 0;
     }
   }
-});
+} );
 
 var insert = '<input type="text" class="input save-text"><i class="fa fa-floppy-o" aria-hidden="true"></i>';
 var done = '<i class="fa fa-pencil" aria-hidden="true"></i>';
-var pencils = document.getElementsByClassName("fa-pencil");
+var pencils = document.getElementsByClassName( "fa-pencil" );
 
-for (var x = 0; x < pencils.length; x++) {
-  pencils[x].addEventListener("click", createEditField);
+for ( var x = 0; x < pencils.length; x++ ) {
+  pencils[ x ].addEventListener( "click", createEditField );
 }
 
-var createEditField = function(e) {
+var createEditField = function( e ) {
   e.preventDefault();
   var parent = this.parentNode;
   parent.innerHTML = insert;
-  parent.childNodes[0].focus();
-  parent.childNodes[1].addEventListener("click", function(e) {
+  parent.childNodes[ 0 ].focus();
+  parent.childNodes[ 1 ].addEventListener( "click", function( e ) {
     e.preventDefault();
     var parent = this.parentNode;
-    ajax({
+    ajax( {
       url: "/student/profile",
       method: "POST",
       data: data,
-      success: function(res) {
-        res = JSON.parse(res);
-        if (res.success) {
+      success: function( res ) {
+        res = JSON.parse( res );
+        if ( res.success ) {
           parent.innerHTML = change + done;
         } else {
-          document.getElementById("error").innerHTML = res.message;
+          document.getElementById( "error" ).innerHTML = res.message;
         }
       }
-    });
-  });
+    } );
+  } );
 };
 
-var variable = document.getElementsByClassName("variable");
+var variable = document.getElementsByClassName( "variable" );
 
-for (var x = 0; x < variable.length; x++) {
-  if ((variable[x].innerHTML).trim() === "") {
-    variable[x].innerHTML = insert;
-    variable[x].childNodes[0].focus();
-    variable[x].childNodes[1].addEventListener("click", submitData);
+for ( var x = 0; x < variable.length; x++ ) {
+  if ( ( variable[ x ].innerHTML ).trim() === "" ) {
+    variable[ x ].innerHTML = insert;
+    variable[ x ].childNodes[ 0 ].focus();
+    variable[ x ].childNodes[ 1 ].addEventListener( "click", submitData );
   }
 }
 
-var submitData = function(e) {
+var submitData = function( e ) {
   e.preventDefault();
   var parent = this.parentNode;
-  var change = parent.getElementsByTagName('INPUT')[0].value;
-  if (!change) return;
+  var change = parent.getElementsByTagName( 'INPUT' )[ 0 ].value;
+  if ( !change ) return;
   var data = {};
-  data[parent.getAttribute('id')] = change;
-  ajax({
+  data[ parent.getAttribute( 'id' ) ] = change;
+  ajax( {
     url: "/student/profile",
     method: "POST",
     data: data,
-    success: function(res) {
-      res = JSON.parse(res);
-      if (res.success) {
+    success: function( res ) {
+      res = JSON.parse( res );
+      if ( res.success ) {
         parent.innerHTML = change + done;
-        parent.children[0].addEventListener('click', createEditField);
+        parent.children[ 0 ].addEventListener( 'click', createEditField );
       } else {
-        document.getElementById("error").innerHTML = res.message;
+        document.getElementById( "error" ).innerHTML = res.message;
       }
     }
-  });
+  } );
 };
 
-document.getElementById("submit").addEventListener("click", function(e) {
+document.getElementById( "submit" ).addEventListener( "click", function( e ) {
   e.preventDefault();
   var data = {};
-  for (var x = 1; x < 11; x++) {
-    data[x] = [document.getElementById(x + "name").children[0].children[1].value, document.getElementById(x + "code").value];
+  for ( var x = 1; x < 11; x++ ) {
+    data[ x ] = [ document.getElementById( x + "name" ).children[ 0 ].children[ 1 ].value, document.getElementById( x + "code" ).value ];
   }
-  ajax({
+  ajax( {
     url: "/student/profile",
     method: "POST",
     data: data,
-    success: function(res) {
-      res = JSON.parse(res);
-      if (res.success) {} else {
-        document.getElementById("error").innerHTML = res.message;
+    success: function( res ) {
+      res = JSON.parse( res );
+      if ( res.success ) {} else {
+        document.getElementById( "error" ).innerHTML = res.message;
       }
     }
-  });
-});
+  } );
+} );
