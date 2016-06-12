ccf9ff1b95040bf8502df64ec347b1ad778b66c5
Leon Chou <bleepbloopsify@gmail.com>
2016-06-12 00:40:58 -0400

    pls

--- public/js/profile.js
+++ public/js/profile.js
@@ -17,7 +17,7 @@ ajax( {
       } );
       input.parentNode.parentNode.childNodes[ 5 ].childNodes[ 1 ].addEventListener( "keydown", function( e ) {
         if ( event.keyCode == 9 ) {
-          console.log(input.parentNode.parentNode.nextElementSibling.children[1].children[0]);
+          input.parentNode.parentNode.nextElementSibling.children[1].children[0].children[0].children[1].focus();
 
           // input.parentNode.parentNode.childNodes[ 5 ].childNodes[ 1 ].focus();
         }
