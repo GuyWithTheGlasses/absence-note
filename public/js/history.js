b704c5a21b2ad53ca04221ac6c9f20adfceb3fd5
Leon Chou <bleepbloopsify@gmail.com>
2016-06-16 11:55:20 -0400

    ps

--- public/js/history.js
+++ public/js/history.js
@@ -1,14 +1,10 @@
 var rows = document.getElementsByClassName( "tablerow" );
-
-for ( var x = 0; x < rows.length; x++ ) {
-  var link = rows[ x ].children[ 0 ].children[ 0 ].getAttribute( "href" );
-  var cells = rows[ x ].children;
-  for ( var y = 0; y < cells.length; y++ ) {
-    if ( cells[ y ].classList.contains( "tablelink" ) ) {
-      cells[ y ].addEventListener( "click", function( e ) {
+forEachInClass(document, 'tablerow', function(row){
+    forEachInClass(row, 'tablelink', function(element){
+      element.addEventListener('click', function(e){
         e.preventDefault();
-        window.location = link;
-      } );
-    }
-  }
-}
+        var link = row.children[0].children[0].getAttribute('href');
+        window.location.href = link;
+      });
+    });
+});
